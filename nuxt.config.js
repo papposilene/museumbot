bd56d08a6c1fb06ba1bc58fe80b198f5afcda74d
Philippe-Alexandre Pierre <philippealexandre.pierre@gmail.com>
2021-03-02 09:17:40 +0100

    NuxtJS frontend

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,25 @@ export default {
         '@nuxtjs/color-mode',
         '@nuxtjs/tailwindcss',
     ],
+    colorMode: {
+        classSuffix: ''
+    },
+    head: {
+        title: 'MuseumBots',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Bunch of projects of small Twitter bots about culture, museums and heritage.'
+          }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        bodyAttrs: {
+            class: 'bg-white dark:bg-black'
+        }
+    },
     ssr: false,
     target: 'static'
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,44 @@
-<template>
-  <main class="w-full max-h-full bg-black">
-    <div class="flex flex-col justify-center mx-auto p-5 w-full xl:w-1/2">
-      <h1 class="bg-red-500 font-black mt-4 p-4 text-3xl uppercase">
-        MuseumBots
-      </h1>
-      <h2 class="p-4 text-sm text-right">
-        Encore un <a href="https://github.com/papposilene/museumbot" class="no-underline hover:underline" target="_blank" rel="nooepener">bien beau projet de Philippe-Alexandre</a>.
-      </h2>
-      <ul>
-        <li class="bg-gray-200 mb-4 p-4">
-          <span class=text-xl>
-            <a href="https://twitter.com/louvrobot" class="font-bold no-underline hover:underline" target="_blank" rel="nooepener">LouvrBot</a>
-          </span><br />
-          <span class="text-sm">
-            Je suis un bot qui tweete des oeuvres du <a href="https://www.louvre.fr/" class="no-underline hover:underline" target="_blank" rel="nooepener">@museelouvre</a>.
-            N'hésitez pas à venir les admirer dans les salles !
-          </span>
-        </li>
-        <li class="bg-gray-200 mb-4 p-4">
-          <span class=text-xl>
-            <a href="https://twitter.com/patchedmet" class="font-bold no-underline hover:underline" target="_blank" rel="nooepener">PatchedMET</a>
-          </span><br />
-          <span class="text-sm">
-            A bot by <a href="https://twitter.com/papposilene" class="no-underline hover:underline" target="_blank" rel="nooepener">@papposilene</a> that combines great artworks from the
-            <a href="https://www.metmuseum.org/" class="no-underline hover:underline" target="_blank" rel="nooepener">Metropolitan Museum of Art</a> with video games patch notes
-            (Crusader Kings 3, World of Warcraft, etc.).
-            Inspired by <a href="https://twitter.com/litpatches_txt" class="no-underline hover:underline" target="_blank" rel="nooepener">@litpatches_txt</a>,
-            created by <a href="https://twitter.com/lizengland" class="no-underline hover:underline" target="_blank" rel="nooepener">@lizengland</a>.
-          </span>
-        </li>
-      </ul>
-      <h5 class="p-4 text-xs text-right">
-        Frontend with NuxtJS and Tailwind CSS.
-      </h5>
-    </div>
+<template class="bg-black">
+  <main class="flex flex-col justify-center mx-auto p-5 w-full xl:w-1/2">
+    <h1 class="bg-gray-300 mb-4 mt-4 p-4 rounded">
+      <span class="font-black text-3xl uppercase">MuseumBots</span><br />
+      <span class="text-sm">
+        Projets de petits bots Twitter culturels, muséaux et patrimoniaux.<br />
+        Projects of small Twitter bots about culture, museums and heritage.
+      </span>
+    </h1>
+    <ul>
+      <li class="bg-gray-100 mb-4 p-4 rounded">
+        <span class=text-xl>
+          <a href="https://twitter.com/louvrobot" class="font-bold no-underline hover:underline" target="_blank" rel="nooepener">LouvrBot</a>
+        </span><br />
+        <span class="text-sm">
+          Je suis un bot qui tweete des oeuvres du <a href="https://www.louvre.fr/" class="no-underline hover:underline" target="_blank" rel="nooepener">@museelouvre</a>.
+          N'hésitez pas à venir les admirer dans les salles !
+        </span>
+      </li>
+      <li class="bg-gray-100 mb-4 p-4 rounded">
+        <span class=text-xl>
+          <a href="https://twitter.com/patchedmet" class="font-bold no-underline hover:underline" target="_blank" rel="nooepener">PatchedMET</a>
+        </span><br />
+        <span class="text-sm">
+          A bot by <a href="https://twitter.com/papposilene" class="no-underline hover:underline" target="_blank" rel="nooepener">@papposilene</a> that combines great artworks from the
+          <a href="https://www.metmuseum.org/" class="no-underline hover:underline" target="_blank" rel="nooepener">Metropolitan Museum of Art</a> with video games patch notes
+          (Crusader Kings 3, The Sims 4, World of Warcraft, etc.).
+          Inspired by <a href="https://twitter.com/litpatches_txt" class="no-underline hover:underline" target="_blank" rel="nooepener">@litpatches_txt</a>,
+          created by <a href="https://twitter.com/lizardengland" class="no-underline hover:underline" target="_blank" rel="nooepener">@lizardengland</a>.
+        </span>
+      </li>
+    </ul>
+    <h5 class="text-black dark:text-white p-4 text-xs text-right">
+      Encore un <a href="https://github.com/papposilene/museumbot" class="no-underline hover:underline" target="_blank" rel="nooepener">bien beau projet de Philippe-Alexandre</a>.<br />
+      Frontend with NuxtJS and Tailwind CSS.
+    </h5>
   </main>
 </template>
+
+<script>
+export default {
+  colorMode: 'dark',
+}
+</script>
